ff1e75f3b84f71a86d43217554c15a985c75de15
José Cabrera <ay35whi@hotmail.com>
2019-09-30 23:29:42 -0400

    Se corrije url del build

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import config from './config'
 
 const { apiKey } = config;
-const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`;
+const url = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`;
 
 export default function getArtists(country) {
 	const URL = url.replace(':country',country);
