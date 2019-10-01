0671c38e9ec0c56f479158d64fed1cb1d74cc27e
José Cabrera <ay35whi@hotmail.com>
2019-09-30 23:15:04 -0400

    Innitial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: 'c11b6dcf6b4f3519a5e067b1486d94d2',
+	secret: '9d9ea23e7e6b8c04b3eef877b57f5a1d',
+	registered_to: 'josedaian'
+}
+
+/*Application name	platzimusic
+API key	c11b6dcf6b4f3519a5e067b1486d94d2
+Shared secret	9d9ea23e7e6b8c04b3eef877b57f5a1d
+Registered to	josedaian*/
---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+import config from './config'
+
+const { apiKey } = config;
+const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`;
+
+export default function getArtists(country) {
+	const URL = url.replace(':country',country);
+	return fetch(URL)
+		.then(res => res.json())
+		.then(json => json.topartists.artist)
+}
